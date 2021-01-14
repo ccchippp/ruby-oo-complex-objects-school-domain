3cb2729363154f98a65a262db21fe2ccab1b1104
ccchippp <clempke90@gmail.com>
2021-01-13 20:03:00 -0500

    need to sort students by grades to pass

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require "pry"
+
+class School
+    
+    attr_accessor :roster
+    
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+    
+    def add_student(student_name, student_grade)
+        @roster[student_grade] ||= []
+        @roster[student_grade] << student_name
+    end
+    
+    def grade(num)
+        @roster[num]
+    end 
+
+    def sort
+        @roster.keys.sort
+    end
+end
